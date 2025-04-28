af61d28b037813e8011f992504b5f6d27c942e8f
Alexandar Georgiev <georgievalex807@gmail.com>
2025-04-29 00:37:44 +0300

    feat:Havent upload them right

--- src/PO2RemoveElement27.java
+++ src/PO2RemoveElement27.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import com.sun.jdi.PathSearchingVirtualMachine;
+
+import java.io.FilterOutputStream;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class PO2RemoveElement27 {
+    public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+         int [] nums={0,1,2,2,3,0,4,2};
+        System.out.println(removeElement(nums,2));
+
+    }
+    public  static int removeElement(int[] nums, int val) {
+        int k=0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i]!=val){
+
+                nums[k]=nums[i];
+                k++;
+            }
+
+        }
+        return k;
+    }
 }
